66f2d1f492206ba6a205ec209a96711415b04708
kavinda0126 <127762257+kavinda0126@users.noreply.github.com>
2024-05-12 00:50:24 +0530

    delete functionality implemented

--- app/src/main/java/com/example/taskapp/Adapter/TaskAdapter.kt
+++ app/src/main/java/com/example/taskapp/Adapter/TaskAdapter.kt
@@ -7,9 +7,19 @@ import androidx.recyclerview.widget.RecyclerView.Adapter
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import com.example.taskapp.R
 import com.example.taskapp.ViewHolder.TaskVH
+import com.example.taskapp.database.Task
+import com.example.taskapp.database.TaskRepository
+import com.example.taskapp.viewModel.MainActivityData
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
-class TaskAdapter:Adapter<TaskVH>() {
-    lateinit var context:Context
+class TaskAdapter(items:List<Task>,repository: TaskRepository,viewModel:MainActivityData):Adapter<TaskVH>() {
+    var context:Context?=null
+    val items=items
+    val repository=repository
+    val viewModel=viewModel
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TaskVH {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.view_item,parent,false)
         context=parent.context
@@ -18,11 +28,23 @@ class TaskAdapter:Adapter<TaskVH>() {
     }
 
     override fun getItemCount(): Int {
-        return 10
+        return items.size
     }
 
     override fun onBindViewHolder(holder: TaskVH, position: Int) {
-        holder.titleinput.text="Sample Title"
+        holder.titleinput.text=items.get(position).title
+        holder.dateinput.text=items.get(position).date
+        holder.timeinput.text=items.get(position).time
+        holder.statusinput.text=items.get(position).status
 
+        holder.deletebtn.setOnClickListener{
+            CoroutineScope(Dispatchers.IO).launch {
+                repository.delete(items.get(position))
+                val data=repository.getAllItems()
+                withContext(Dispatchers.Main){
+                    viewModel.setData(data)
+                }
+            }
+        }
     }
 }
